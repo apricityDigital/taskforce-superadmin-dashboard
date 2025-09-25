22665f7158b3d9a4c50ccef7e59cf14fddb427d3
Ashutosh Parakh <ashu70056@gmail.com>
2025-09-25 12:07:34 +0530

    reupdated with commit changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/taskforce-superadmin-dashboard',
-  assetPrefix: '/taskforce-superadmin-dashboard/', // ✅ Important for GitHub Pages
+  
   images: {
     unoptimized: true,
   },
